f21b2738417888bb82d17f8df31fe5a7b9a0a2f0
Matt Lyell <mlyell21@Gmail.com>
2022-11-28 19:34:42 -0800

    rubocop circular buffer

--- circular_buffer/circular_buffer.rb
+++ circular_buffer/circular_buffer.rb
@@ -1,11 +1,11 @@
-#class CircularBuffer
+# class CircularBuffer
 #  instance methods
 #    - initialize  - input is an integer which sets ivar of buffer size
 #    - read - input: ivar output: removes oldes value from the buffer
 #    - write - input: string
 #    - write! - overrides fullbuffer and removes oldest, writing a new value
 #    - clear - returns a new empty buffer
- 
+
 # subclass
 #   BufferEmptyException - for error handling, if a buffer is empty, this exception should be thrown
 #     - this is namespaced from CircularBuffer::BufferEmptyException
@@ -18,22 +18,19 @@
 # if an element is overwritten with a string value, when the next write happens
 # if nil is written, it does not count as buffer space, and the next element will be written at the same location that nil is written on
 
-
-# data 
+# data
 #  array object
 
 # algorithm
-# create an array that has n number of spaces that are available.    
+# create an array that has n number of spaces that are available.
 # if the index is greater than that number, the first element in the array should be dropped to make room for the new element
-
-
 
 class CircularBuffer
   def initialize(size)
     @buffer = []
     @size = size
   end
- 
+
   def read
     @buffer.empty? ? raise(BufferEmptyException) : @buffer.shift
   end
@@ -45,20 +42,19 @@ class CircularBuffer
 
   def write!(value)
     return if value.nil?
-    self.read if buffer_full?
-    self.write(value)
-  end 
 
-  def clear 
+    read if buffer_full?
+    write(value)
+  end
+
+  def clear
     @buffer = []
   end
 
   def buffer_full?
     @buffer.size == @size
-  end 
-  
+  end
 
   class BufferFullException < StandardError; end
   class BufferEmptyException < StandardError; end
 end
-
